dba3bb804a2756a1f4f2f24db733612d2074da1b
shahisachin917 <109494041+shahisachin917@users.noreply.github.com>
2023-03-18 22:45:32 -0500

    Removed react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
@@ -36,26 +36,26 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route
-              exact
-              path="/"
-              element={
-                <TextForm
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                  showAlert={showAlert}
-                />
-              }
-            ></Route>
-          </Routes>
-        </div>
-      </Router>
+      {/* <Router> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />}></Route> */}
+        {/* <Route */}
+        {/* exact
+          path="/"
+          element={ */}
+        <TextForm
+          heading="Enter the text to analyze below"
+          mode={mode}
+          showAlert={showAlert}
+        />
+        {/* } */}
+        {/* ></Route> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
